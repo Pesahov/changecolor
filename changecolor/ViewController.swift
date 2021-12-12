8bb62297cb3286a1f372335271b246127df33e51
Shilol Pesahov <pesahov777@gmail.com>
2021-12-12 14:54:29 +0200

    change color version 1

--- changecolor/ViewController.swift
+++ changecolor/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mainView.layer.cornerRadius = 10
+        
+        redLabel.font = redLabel.font.withSize(17)
+        greenLabel.font = greenLabel.font.withSize(17)
+        blueLabel.font = blueLabel.font.withSize(17)
+        
+        redSlider.value = 0.3
+        greenSlider.value = 0.5
+        blueSlider.value = 0.8
+        
+        redLabel.text = String(redSlider.value)
+        greenLabel.text = String(greenSlider.value)
+        blueLabel.text = String(blueSlider.value)
+        
     }
 
+    @IBAction func rgbColor() {
+        redLabel.text = String(redSlider.value)
+        greenLabel.text = String(greenSlider.value)
+        blueLabel.text = String(blueSlider.value)
+        
+        let colorRed = CGFloat(redSlider.value)
+        let colorGreen = CGFloat(greenSlider.value)
+        let colorBlue = CGFloat(blueSlider.value)
+    
+        mainView.backgroundColor = UIColor(red: colorRed, green: colorGreen, blue: colorBlue, alpha: 1)
+    }
 
 }
+
 
